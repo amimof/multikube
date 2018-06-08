fac6c8c7fb0f62b1f46abc4a36859412f854e6d4
Amir Mofasser <amir.mofasser@gmail.com>
2018-06-08 17:10:03 +0200

    Added main package

---
+++ cmd/multikube/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"os"
+	//"time"
+	"github.com/spf13/pflag"
+	"gitlab.com/amimof/multikube"
+	"gitlab.com/amimof/multikube/api/v1"
+)
+
+func main() {
+
+	pflag.Usage = func() {
+		fmt.Fprint(os.Stderr, "Usage:\n")
+		fmt.Fprint(os.Stderr, "  multikube-server [OPTIONS]\n\n")
+
+		title := "Kubernetes multi-cluster manager"
+		fmt.Fprint(os.Stderr, title+"\n\n")
+		desc := "Manages multiple Kubernetes clusters and provides a single API to clients"
+		if desc != "" {
+			fmt.Fprintf(os.Stderr, desc+"\n\n")
+		}
+		fmt.Fprintln(os.Stderr, pflag.CommandLine.FlagUsages())
+	}
+
+	// parse the CLI flags
+	pflag.Parse()
+	
+	// Create the api
+	a := v1.NewAPI()
+
+	// Create the server
+	s := multikube.NewServer(a)
+
+	err := s.Serve() 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
